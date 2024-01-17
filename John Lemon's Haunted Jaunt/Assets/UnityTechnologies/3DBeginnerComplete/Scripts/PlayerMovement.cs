570563ed1a46deb65d838bd066efdc1e0b8cd9de
WhosKirito <dominickhawkins738@gmail.com>
2024-01-17 13:30:18 -0800

    Finished Lesson 3 and started 4

--- John Lemon's Haunted Jaunt/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
+++ John Lemon's Haunted Jaunt/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
     void Start ()
     {
         m_Animator = GetComponent<Animator> ();
-        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_Rigidbody = GetComponent<Rigidbody> (); 
         m_AudioSource = GetComponent<AudioSource> ();
         
         MoveAction.Enable();
---
+++ John Lemon's Haunted Jaunt/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/_.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class _ : MonoBehaviour
+{
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+         float vertical = Input.GetAxis("Vertcal");
+
+        
+        
+    }
+}
